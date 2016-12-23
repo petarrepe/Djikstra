4a0606d0dc85a6e45014a7bfe5138515663cdf2b
Petar Stojanović <petar.stojanovic@fer.hr>
2016-12-23 14:17:11 +0100

    Add project files.

---
+++ Djikstra/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Djikstra.Startup))]
+namespace Djikstra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
